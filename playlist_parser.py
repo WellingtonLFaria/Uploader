f4bf9a63762341ae64cc1a0f3c0ce3159be59050
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-11-01 10:21:48 -0300

    refactor: creating functions to all script processes

--- playlist_parser.py
+++ playlist_parser.py
@@ -4,6 +4,63 @@ from json import dumps
 
 PATH = sys.argv[1]
 
+class Playlist:
+    def __init__(self, name: str, path: str) -> None:
+        self.name = name
+        self.path = path
+        self.videos = []
+
+    def add_videos(self) -> None:
+        for item in os.listdir(self.path):
+            if is_a_video(item):
+                self.videos.append(item)
+    
+    def get_videos(self, path: str):
+        for item in os.listdir(path):
+            if is_a_folder(f"{path}/{item}"):
+                self.get_videos(f"{path}/{item}")
+            else:
+                if is_a_video(item):
+                    self.videos.append(Video(item, f"{path}/{item}").__dict__)
+
+class Video:
+    def __init__(self, name: str, path: str) -> None:
+        self.name = name
+        self.path = path
+
+def run():
+    playlists = create_playlists()
+    playlists = get_playlists_videos(playlists)
+    save_playlists(playlists)
+
+def create_playlists() -> list[Playlist]:
+    playlists = [Playlist("blank", PATH)]
+    for item in os.listdir(PATH):
+        if is_a_folder(f"{PATH}/{item}"):
+            playlists.append(Playlist(item, f"{PATH}/{item}"))
+    return playlists
+
+def get_playlists_videos(playlists: list[Playlist]) -> list[Playlist]:
+    for playlist in playlists:
+        if playlist.name == "blank":
+            for item in os.listdir(playlist.path):
+                if is_a_video(item):
+                    playlist.videos.append(Video(item, f"{playlist.path}/{item}").__dict__)
+        else:
+            playlist.get_videos(playlist.path)
+    return playlists
+
+def save_playlists(playlists: list[Playlist]) -> None:
+    with open("playlist.json", "w", encoding="utf-8") as file:
+        file.write('{"playlists":[')
+        length = len(playlists)
+        for playlist in playlists:
+            if playlists.index(playlist) == length - 1:
+                file.write(f"{dumps(playlist.__dict__)}")
+            else:
+                file.write(f"{dumps(playlist.__dict__)},")
+        file.write("]}")
+
 def is_a_folder(path: str):
     try:
         if path.split("/")[-1][0] == ".":
@@ -23,53 +80,5 @@ def is_a_video(file: str) -> bool:
     except:
         return False
 
-class Playlist:
-    def __init__(self, name: str, path: str) -> None:
-        self.name = name
-        self.path = path
-        self.videos = []
-
-    def add_videos(self) -> None:
-        for item in os.listdir(self.path):
-            if is_a_video(item):
-                self.videos.append(item)
-    
-    def get_videos(self, path):
-        for item in os.listdir(path):
-            if is_a_folder(f"{path}/{item}"):
-                self.get_videos(f"{path}/{item}")
-            else:
-                if is_a_video(item):
-                    self.videos.append(Video(item, f"{path}/{item}").__dict__)
-
-class Video:
-    def __init__(self, name: str, path: str) -> None:
-        self.name = name
-        self.path = path
-
-
-def run():
-    playlists = [Playlist("blank", PATH)]
-    for item in os.listdir(PATH):
-        if is_a_folder(f"{PATH}/{item}"):
-            playlists.append(Playlist(item, f"{PATH}/{item}"))
-
-    for playlist in playlists:
-        if playlist.name == "blank":
-            for item in os.listdir(playlist.path):
-                if is_a_video(item):
-                    playlist.videos.append(Video(item, f"{playlist.path}/{item}").__dict__)
-        else:
-            playlist.get_videos(playlist.path)
-        print(playlist.__dict__)
-    
-    with open("playlist.json", "w") as file:
-        file.write('{"playlists":[')
-        tamanho = len(playlists)
-        for playlist in playlists:
-            if playlists.index(playlist) == tamanho - 1:
-                file.write(f"{dumps(playlist.__dict__)}")
-            else:
-                file.write(f"{dumps(playlist.__dict__)},")
-        file.write("]}")
-run()
+if __name__ == "__main__":
+    run()
